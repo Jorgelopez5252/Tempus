afb200508eecc5f56de66c9039819c32cf1206ab
Martin Stovall <martinstovall@yahoo.com>
2019-11-29 01:15:16 -0700

    schema file

---
+++ schema.sql
@@ -0,0 +1,230 @@
+DROP SCHEMA
+
+IF EXISTS hr;
+	CREATE SCHEMA hr COLLATE = utf8_general_ci;
+
+USE hr;
+
+/* *************************************************************** 
+***************************CREATING TABLES************************
+**************************************************************** */
+
+
+CREATE TABLE jobs (
+	job_id VARCHAR(10) NOT NULL,
+	job_title VARCHAR(35) NOT NULL,
+	min_salary DECIMAL(8, 0) UNSIGNED,
+	max_salary DECIMAL(8, 0) UNSIGNED,
+	PRIMARY KEY (job_id)
+	);
+
+CREATE TABLE employees (
+	employee_id INT (11) UNSIGNED NOT NULL,
+	first_name VARCHAR(20),
+	last_name VARCHAR(25) NOT NULL,
+	email VARCHAR(25) NOT NULL,
+	phone_number VARCHAR(20),
+	hire_date DATE NOT NULL,
+	job_id VARCHAR(10) NOT NULL,
+	salary DECIMAL(8, 2) NOT NULL,
+	manager_id INT (11) UNSIGNED,
+	PRIMARY KEY (employee_id)
+	);
+
+
+CREATE VIEW emp_details_view
+AS
+SELECT e.employee_id,
+	e.job_id,
+	e.manager_id,
+	e.first_name,
+	e.last_name,
+	e.salary,
+	j.job_title,
+	l.city,
+	l.state_province,
+	
+FROM employees e, 
+jobs j,
+	
+WHERE j.job_id = e.job_id;
+
+/* *************************************************************** 
+***************************INSERTING DATA*************************
+**************************************************************** */
+SET FOREIGN_KEY_CHECKS = 0;
+
+INSERT INTO jobs
+VALUES (
+	'AD_PRES',
+	'President',
+	20000,
+	40000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'AD_VP',
+	'Administration Vice President',
+	15000,
+	30000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'AD_ASST',
+	'Administration Assistant',
+	3000,
+	6000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'FI_MGR',
+	'Finance Manager',
+	8200,
+	16000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'FI_ACCOUNT',
+	'Accountant',
+	4200,
+	9000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'AC_MGR',
+	'Accounting Manager',
+	8200,
+	16000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'AC_ACCOUNT',
+	'Public Accountant',
+	4200,
+	9000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'SA_MAN',
+	'Sales Manager',
+	10000,
+	20000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'SA_REP',
+	'Sales Representative',
+	6000,
+	12000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'PU_MAN',
+	'Purchasing Manager',
+	8000,
+	15000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'PU_CLERK',
+	'Purchasing Clerk',
+	2500,
+	5500
+	);
+
+INSERT INTO jobs
+VALUES (
+	'ST_MAN',
+	'Stock Manager',
+	5500,
+	8500
+	);
+
+INSERT INTO jobs
+VALUES (
+	'ST_CLERK',
+	'Stock Clerk',
+	2000,
+	5000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'SH_CLERK',
+	'Shipping Clerk',
+	2500,
+	5500
+	);
+
+INSERT INTO jobs
+VALUES (
+	'IT_PROG',
+	'Programmer',
+	4000,
+	10000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'MK_MAN',
+	'Marketing Manager',
+	9000,
+	15000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'MK_REP',
+	'Marketing Representative',
+	4000,
+	9000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'HR_REP',
+	'Human Resources Representative',
+	4000,
+	9000
+	);
+
+INSERT INTO jobs
+VALUES (
+	'PR_REP',
+	'Public Relations Representative',
+	4500,
+	10500
+	);
+
+COMMIT;
+
+INSERT INTO employees
+VALUES (
+	100,
+	'Steven',
+	'King',
+	'SKING@ysu.com', 
+	'515.123.4567',
+	STR_TO_DATE('17-JUN-1987', '%d-%M-%Y'),
+	'AD_PRES',
+	24000,
+	90
+	);
+
+COMMIT;
+
+/* *************************************************************** 
+***************************FOREIGN KEYS***************************
+**************************************************************** */
+
+ALTER TABLE employees ADD FOREIGN KEY (job_id) REFERENCES jobs(job_id);
