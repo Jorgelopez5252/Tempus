8a989b11daf6f8b2caad0169915bde703bc000a5
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-12-03 13:19:20 -0700

    added a seeds folder with the right key values

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,10 @@
-INSERT INTO users (name) VALUES('Test');
-INSERT INTO users (name) VALUES('Janet');
-INSERT INTO users (name) VALUES('Hank');
-INSERT INTO users (name) VALUES('Peggy');
-INSERT INTO users (name) VALUES('John');
+USE userDB;
+
+INSERT INTO usersTable (first_name, last_name, title, salary)
+VALUES('Test','tester', 'admin', '80000'),
+('john','smith', 'employee', '30000'),
+('happy','gillmore','employee', '90000'),
+('marcus','fenix', 'employee', '28000'),
+('alex','mason', 'employee', '80000');
+
+
