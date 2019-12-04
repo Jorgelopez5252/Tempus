421f20d117df792dee8c940ea8d76f8e3ded17ba
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-12-04 14:00:10 -0700

    added price html with a css linked in under public dir.

--- seeds.sql
+++ seeds.sql
@@ -1,10 +1,10 @@
-USE userDB;
+-- USE userDB;
 
-INSERT INTO usersTable (first_name, last_name, title, salary)
-VALUES('Test','tester', 'admin', '80000'),
-('john','smith', 'employee', '30000'),
-('happy','gillmore','employee', '90000'),
-('marcus','fenix', 'employee', '28000'),
-('alex','mason', 'employee', '80000');
+-- INSERT INTO users (firstname, lastname, title, salary, password)
+-- VALUES('Test','tester', 'admin', '80000','test'),
+-- ('john','smith', 'employee', '30000','test'),
+-- ('happy','gillmore','employee', '90000','test'),
+-- ('marcus','fenix', 'employee', '28000','test'),
+-- ('alex','mason', 'employee', '80000','test');
 
 
